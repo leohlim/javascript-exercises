dbd0a49da094c3a045f3b1d21f30d54ef23cc407
leohlim <leohlim@gmail.com>
2022-08-13 23:30:35 -0500

    Completed calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,30 @@
-const add = function() {
+const add = function(a, b) {
+	return a + b;
+};
+
+const subtract = function(a, b) {
+	return a - b;
+};
+
+const sum = function(arr) {
+  const initialValue = 0;
+  return arr.reduce((previousValue, currentValue) => previousValue + currentValue, initialValue)
 	
 };
 
-const subtract = function() {
-	
+const multiply = function(arr) {
+  return arr.reduce((a, b) => {return a * b});
 };
 
-const sum = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
+const factorial = function fact(n) {
+  if (n===0 || n===1) {
+    return 1;
+  }
+  return n*fact(n-1);
 	
 };
 
